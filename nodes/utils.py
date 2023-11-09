d0e97032ef3bc10bec0f3646aec43bfb8eac54e8
marcojoao <marcojoao11@gmail.com>
2023-11-09 17:29:33 +0000

    fix stack

--- nodes/utils.py
+++ nodes/utils.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 
 def image_array_to_torch(image_np):
+    out_list = []
     img = image_np.get_float_value().transpose((2, 0, 1))
-    img = np.expand_dims(img, axis=0)
-    return torch.tensor(img, dtype=torch.float32)
+    out_list.append(torch.tensor(img, dtype=torch.float32))
+    return torch.stack(out_list)
