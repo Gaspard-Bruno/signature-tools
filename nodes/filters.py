19775fd717b7f34070534bb077e557cec82c210f
marcojoao <marcojoao11@gmail.com>
2023-11-09 21:27:35 +0000

    add UnsharpMask

--- nodes/filters.py
+++ nodes/filters.py
@@ -1,6 +1,6 @@
 from .utils import *
 from .categories import FILTER_CAT
-from kornia.filters import gaussian_blur2d, canny, laplacian
+from kornia.filters import gaussian_blur2d, unsharp_mask, canny, laplacian
 import torch
 
 class GaussianBlur:
@@ -33,6 +33,35 @@ class GaussianBlur:
                                  sigma=in_sigma,
                                  border_type='reflect',
                                  separable=True)
+        output = output.transpose(3, 1)
+        return (output,)
+
+class UnsharpMask:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s): # type: ignore
+        return {"required": {"image": ("IMAGE",),
+                             "kernel_width": ("INT", {"default": 3}),
+                             "kernel_height": ("INT", {"default": 3}),
+                             "sigma_x": ("FLOAT", {"default": 1.5}),
+                             "sigma_y": ("FLOAT", {"default": 1.5}),
+                             }
+                }
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "process"
+    CATEGORY = FILTER_CAT
+
+    def process(self, image: torch.Tensor, kernel_width, kernel_height, sigma_x, sigma_y):
+        if kernel_width % 2 == 0:
+            kernel_width += 1
+        if kernel_height % 2 == 0:
+            kernel_height += 1
+        in_kernel_size = (kernel_width, kernel_height)
+        in_sigma = (sigma_x, sigma_y)
+        image = image.transpose(3, 1)
+        output = unsharp_mask(image, kernel_size=in_kernel_size, sigma=in_sigma)
         output = output.transpose(3, 1)
         return (output,)
 
@@ -86,6 +115,7 @@ class Laplacian:
 
 NODE_CLASS_MAPPINGS = {
     "Gaussian Blur": GaussianBlur,
+    "UnsharpMask": UnsharpMask,
     "Canny Edge": CannyEdge,
     "Laplacian": Laplacian,
 }
