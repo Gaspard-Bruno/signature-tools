8aa62e4c542dca261b3b3ee586ade305f92a5e7a
marcojoao <marcojoao11@gmail.com>
2023-11-09 18:26:34 +0000

    add adjust saturation

--- nodes/image_array_node.py
+++ nodes/image_array_node.py
@@ -1,7 +1,7 @@
 from  ..src.signature.img.image_array import ImageArray
 from .utils import *
 from .categories import *
-from kornia.enhance import adjust_brightness
+from kornia.enhance import adjust_brightness, adjust_hue, adjust_saturation
 import torch
 class ImageFromWeb:
     def __init__(self):
@@ -11,9 +11,9 @@ class ImageFromWeb:
     def INPUT_TYPES(s): # type: ignore
         return {"required": {"url": ("STRING", {"default": "URL HERE"})}}
     RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "image_from_web"
+    FUNCTION = "process"
     CATEGORY = IMAGE_CAT
-    def image_from_web(self, url):
+    def process(self, url):
         img_arr = ImageArray.from_web(url)
         image = img_np_to_tensor([img_arr.get_value()])
         return (image,)
@@ -29,16 +29,36 @@ class AdjustBrightness:
                             }
                 }
     RETURN_TYPES = ("IMAGE",)
-    FUNCTION = "img_adjust_brightness"
+    FUNCTION = "process"
     CATEGORY = IMAGE_CAT
 
-    def img_adjust_brightness(self, image, factor):
+    def process(self, image, factor):
         output = adjust_brightness(image, np.clip(factor, 0.0, 1.0), clip_output=True)
         # output = torch.stack([output])
         return (output,)
 
 
+class AdjustSaturation:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s): # type: ignore
+        return {"required": {"image": ("IMAGE",),
+                             "factor": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
+                            }
+                }
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "process"
+    CATEGORY = IMAGE_CAT
+
+    def process(self, image, factor):
+        output = adjust_saturation(image, np.clip(factor, 0.0, 1.0))
+        # output = torch.stack([output])
+        return (output,)
+
 NODE_CLASS_MAPPINGS = {
     "Load from Web": ImageFromWeb,
     "Adjust Brightness": AdjustBrightness,
+    "Adjust Saturation": AdjustSaturation,
 }
