82b8933883edb99f82a3d3a638b57d7d45bb835c
marcojoao <marcojoao11@gmail.com>
2023-11-09 17:38:13 +0000

    test special charaters

--- nodes/utils.py
+++ nodes/utils.py
@@ -1,15 +1,14 @@
 import torch
 import numpy as np
 
-def image_array_to_torch(image_np):
-    out_list = []
-    img = image_np.get_float_value().transpose((2, 0, 1))
-    out_list.append(torch.tensor(img, dtype=torch.float32))
-    return torch.stack(out_list)
-
-
 def img_np_to_tensor(img_np_list):
     out_list = []
     for img_np in img_np_list:
         out_list.append(torch.from_numpy(img_np.astype(np.float32) / 255.0))
     return torch.stack(out_list)
+
+def img_tensor_to_np(img_tensor):
+    img_tensor = img_tensor.clone()
+    img_tensor = img_tensor * 255.0
+    mask_list = [x.squeeze().numpy().astype(np.uint8) for x in torch.split(img_tensor, 1)]
+    return mask_list
