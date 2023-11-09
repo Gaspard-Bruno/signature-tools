c2c591527f239af0bb6ea8929e99243edb83bb5a
marcojoao <marcojoao11@gmail.com>
2023-11-09 21:18:54 +0000

    add Laplacian

--- nodes/filters.py
+++ nodes/filters.py
@@ -1,6 +1,6 @@
 from .utils import *
 from .categories import FILTER_CAT
-from kornia.filters import gaussian_blur2d, canny
+from kornia.filters import gaussian_blur2d, canny, laplacian
 import torch
 
 class GaussianBlur:
@@ -11,7 +11,7 @@ class GaussianBlur:
     def INPUT_TYPES(s): # type: ignore
         return {"required": {"image": ("IMAGE",),
                              "kernel_width": ("INT", {"default": 3}),
-                             "sigma_height": ("INT", {"default": 3}),
+                             "kernel_height": ("INT", {"default": 3}),
                              "sigma_x": ("FLOAT", {"default": 1.5}),
                              "sigma_y": ("FLOAT", {"default": 1.5}),
                              }
@@ -43,8 +43,8 @@ class CannyEdge:
     @classmethod
     def INPUT_TYPES(s): # type: ignore
         return {"required": {"image": ("IMAGE",),
-                             "low_threshold": ("FLOAT", {"default": 1.5, "min": 0.0, "max": 1.0, "step": 0.01}),
-                             "high_threshold": ("FLOAT", {"default": 1.5, "min": 0.0, "max": 1.0, "step": 0.01}),
+                             "low_threshold": ("FLOAT", {"default": 0.4, "min": 0.0, "max": 1.0, "step": 0.01}),
+                             "high_threshold": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
                              }
                 }
     RETURN_TYPES = ("IMAGE",)
@@ -59,7 +59,33 @@ class CannyEdge:
         return (output,)
 
 
+class Laplacian:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s): # type: ignore
+        return {"required": {"image": ("IMAGE",),
+                             "kernel_width": ("INT", {"default": 3}),
+                             }
+                }
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "process"
+    CATEGORY = FILTER_CAT
+
+    def process(self, image: torch.Tensor, kernel_width, kernel_height, sigma_x, sigma_y):
+        if kernel_width % 2 == 0:
+            kernel_width += 1
+        if kernel_height % 2 == 0:
+            kernel_height += 1
+        in_kernel_size = (kernel_width, kernel_height)
+        image = image.transpose(3, 1)
+        output = laplacian(image, kernel_size=in_kernel_size)
+        output = output.transpose(3, 1)
+        return (output,)
+
 NODE_CLASS_MAPPINGS = {
     "Gaussian Blur": GaussianBlur,
     "Canny Edge": CannyEdge,
+    "Laplacian": Laplacian,
 }
