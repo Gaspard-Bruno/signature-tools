8120e5403f7d49dc77eb3899698433e0412b4892
marcojoao <marcojoao11@gmail.com>
2023-11-09 16:51:05 +0000

    fix return type

--- nodes/image_array_node.py
+++ nodes/image_array_node.py
@@ -1,4 +1,5 @@
 from  ..src.signature.img.image_array import ImageArray
+from .utils import img_np_to_tensor
 
 class ImageFromWeb:
     def __init__(self):
@@ -11,7 +12,8 @@ class ImageFromWeb:
     FUNCTION = "image_from_web"
     CATEGORY = "Signature/Image"
     def image_from_web(self, url):
-        return ImageArray.from_web(url).get_float_value()
+        np_array = ImageArray.from_web(url).get_float_value
+        return img_np_to_tensor(np_array)
 
 
 NODE_CLASS_MAPPINGS = {
