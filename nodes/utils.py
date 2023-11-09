c80968df786c3ef71db7e4776bf2b51b28ed0d7a
marcojoao <marcojoao11@gmail.com>
2023-11-09 17:24:00 +0000

    add dim on axis=0

--- nodes/utils.py
+++ nodes/utils.py
@@ -3,4 +3,5 @@ import numpy as np
 
 def image_array_to_torch(image_np):
     img = image_np.get_float_value().transpose((2, 0, 1))
+    img = np.expand_dims(img, axis=0)
     return torch.tensor(img, dtype=torch.float32)
