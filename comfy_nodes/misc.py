96007ded519673e72aa9e5b8645e1a4bb819c44b
Marco João <marco.joao@gaspardbruno.com>
2023-11-29 11:59:01 +0000

    add mask binary filter update rqs

--- comfy_nodes/misc.py
+++ comfy_nodes/misc.py
@@ -33,7 +33,27 @@ class ZerosLike():
         output_image = TensorImage(step).get_comfy()
         return (output_image,)
 
+class MaskBinaryFilter():
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s): # type: ignore
+        return {"required": {
+            "mask": ("MASK",),
+            }}
+    RETURN_TYPES = ("MASK",)
+    FUNCTION = "process"
+    CATEGORY = MISC_CAT
+    def process(self, mask: torch.Tensor):
+        step = TensorImage.from_comfy(mask)
+        step[step > 0.01] = 1.0
+        step[step <= 0.01] = 0.0
+        output = TensorImage(step).get_comfy()
+        return (output,)
+
 NODE_CLASS_MAPPINGS = {
     "Ones Like": OnesLike,
     "Zeros Like": ZerosLike,
+    "Mask Binary Filter": MaskBinaryFilter,
 }
